9510e518e43517aa9ed90deedd89fb02f4b40da1
rcheney <rcheney@p3s.dev>
2025-05-09 15:23:02 +0100

    Initial server commit.
    Spring start generated project with devtools, Lombok, Data JPA, Spring Web, H2, and PostgreSQL included.

---
+++ src/main/java/dev/p3s/ollamachat/OllamaChatServerApplication.java
@@ -0,0 +1,13 @@
+package dev.p3s.ollamachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OllamaChatServerApplication.class, args);
+    }
+
+}
