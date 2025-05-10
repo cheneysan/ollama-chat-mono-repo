2fc75d368c5bb4272cf162e7bb8ef5203371413e
rcheney <rcheney@p3s.dev>
2025-05-10 14:07:35 +0100

    Missed the ModelEntityMapper from the second to last commit - CI/CD would have caught that before I did.

---
+++ src/main/java/dev/p3s/ollamachat/mapper/ModelEntityMapper.java
@@ -0,0 +1,14 @@
+package dev.p3s.ollamachat.mapper;
+
+import dev.p3s.ollamachat.entity.Chat;
+import dev.p3s.ollamachat.entity.User;
+import dev.p3s.ollamachat.model.ChatSummary;
+import dev.p3s.ollamachat.model.UserSummary;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ModelEntityMapper {
+    UserSummary toSummary(User user);
+
+    ChatSummary toSummary(Chat chat);
+}
