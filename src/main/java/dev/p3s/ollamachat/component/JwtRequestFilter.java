5c30f213143cb94b992c07d40f114190c8843ebc
rcheney <rcheney@p3s.dev>
2025-05-10 14:06:40 +0100

    Setup Spring Security with SecurityConfig bean.
    I'm using JWT tokens (gasp!) which should be generated and passed to the client so that they can be included as the Bearer in future API requests - note that the best practice would be to use OAuth2 and that is on the TODO list.

---
+++ src/main/java/dev/p3s/ollamachat/component/JwtRequestFilter.java
@@ -0,0 +1,52 @@
+package dev.p3s.ollamachat.component;
+
+import dev.p3s.ollamachat.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String email;
+        String token;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+            email = jwtUtil.extractEmail(token);
+        } else {
+            email = null;
+            token = null;
+        }
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            userService.getUserByEmail(email).ifPresent(u -> {
+                if (jwtUtil.validateToken(token, email)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(u, null, Collections.emptySet());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            });
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
